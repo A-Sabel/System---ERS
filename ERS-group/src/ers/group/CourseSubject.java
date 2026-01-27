7b9d198d09c6ccf064079e7f70b3308db3796a1d
Andrea San Andres <A-Sabel@users.noreply.github.com>
2026-01-27 16:33:09 +0800

    Refactor CourseSubject class structure and add missing getters

--- ERS-group/src/ers/group/CourseSubject.java
+++ ERS-group/src/ers/group/CourseSubject.java
@@ -9,5 +9,42 @@ package ers.group;
  * @author Andrea Ysabela
  */
 public class CourseSubject {
-    
+    //Identity
+    private String courseSubjectID;
+    private String courseSubjectName;
+
+    //Academic Constraints
+    private int units;
+    private int studentCount;
+    private boolean isLabRoom;
+    private boolean isScheduled = false;
+
+    //Constructor
+    public CourseSubject(String courseSubjectID, String courseSubjectName, 
+                            int units, int studentCount, boolean isLabRoom) 
+    {
+        this.courseSubjectID = courseSubjectID;
+        this.courseSubjectName = courseSubjectName;
+        this.units = units;
+        this.studentCount = studentCount;
+        this.isLabRoom = isLabRoom;
+    }
+
+    //Getters and Setters
+    public String getCourseSubjectID() {
+        return courseSubjectID;
+    }
+    public String getCourseSubjectName() {
+        return courseSubjectName;
+    }
+    public int getUnits() {
+        return units;
+    }
+    public int getStudentCount() {
+        return studentCount;
+    }
+    public boolean isIsLabRoom() {
+        return isLabRoom;
+    }
+
 }

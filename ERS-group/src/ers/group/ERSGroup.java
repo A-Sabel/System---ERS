5a31aa273764d48925d61d07774be37124ead3ce
Andrea San Andres <A-Sabel@users.noreply.github.com>
2026-01-27 15:39:08 +0800

    add System.out.println("hello");

--- ERS-group/src/ers/group/ERSGroup.java
+++ ERS-group/src/ers/group/ERSGroup.java
@@ -9,12 +9,14 @@ package ers.group;
  * @author Andrea Ysabela
  */
 public class ERSGroup {
+ 
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hello");
     }
     
 }

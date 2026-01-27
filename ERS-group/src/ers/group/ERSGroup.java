ec4f59be7de31a4ba379029eb2dd26d25f13a3f9
Andrea San Andres <A-Sabel@users.noreply.github.com>
2026-01-27 15:34:54 +0800

    hi

---
+++ ERS-group/src/ers/group/ERSGroup.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ers.group;
+
+/**
+ *
+ * @author Andrea Ysabela
+ */
+public class ERSGroup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

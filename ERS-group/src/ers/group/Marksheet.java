b20f1fd9e688ce1332098c0e69b3b1b929a1de42
rice <katricegarnado65@gmail.com>
2026-01-31 13:06:05 +0800

    Add new file 'hu' to the hotdog group

--- ERS-group/src/ers/group/Marksheet.java
+++ ERS-group/src/ers/group/Marksheet.java
@@ -1,5 +1,30 @@
-package ers.group;
-
 public class Marksheet {
 
+    private String studentID;
+    private String studentName;
+    private String schoolYear;
+    private String semester;
+    private String[] subjects; // size = 5
+    private double[] marks;    // size = 5
+
+    // CONSTRUCTOR
+    public Marksheet(String studentID, String studentName,
+                     String schoolYear, String semester,
+                     String[] subjects, double[] marks) {
+
+        this.studentID = studentID;
+        this.studentName = studentName;
+        this.schoolYear = schoolYear;
+        this.semester = semester;
+        this.subjects = subjects;
+        this.marks = marks;
+    }
+
+    // GETTERS
+    public String getStudentID() { return studentID; }
+    public String getStudentName() { return studentName; }
+    public String getSchoolYear() { return schoolYear; }
+    public String getSemester() { return semester; }
+    public String[] getSubjects() { return subjects; }
+    public double[] getMarks() { return marks; }
 }

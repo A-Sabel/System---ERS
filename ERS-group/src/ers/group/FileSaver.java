6bbc5451b88d4ce5e6f9b308c9fab1116f058fb2
Andrea Ysabela San Andres <andrea27ysabela@gmail.com>
2026-02-03 18:16:25 +0800

    Implement ScheduleFileLoader and EnrollmentFileLoader with loading and mapping functionalities

---
+++ ERS-group/src/ers/group/FileSaver.java
@@ -0,0 +1,148 @@
+package ers.group;
+
+public interface FileSaver {
+    void save(String filePath, java.util.List<?> data) throws java.io.IOException;
+}
+
+abstract class BaseFileSaver<T> implements FileSaver {
+    
+    protected abstract String formatLine(T item);
+    
+    @Override
+    public void save(String filePath, java.util.List<?> data) throws java.io.IOException {
+        try (java.io.BufferedWriter writer = new java.io.BufferedWriter(new java.io.FileWriter(filePath))) {
+            for (Object obj : data) {
+                @SuppressWarnings("unchecked")
+                T item = (T) obj;
+                String line = formatLine(item);
+                if (line != null && !line.isEmpty()) {
+                    writer.write(line);
+                    writer.newLine();
+                }
+            }
+        }
+    }
+    
+    protected void update(String filePath, java.util.List<T> data, java.util.function.Predicate<T> predicate, T updatedItem) throws java.io.IOException {
+        for (int i = 0; i < data.size(); i++) {
+            if (predicate.test(data.get(i))) {
+                data.set(i, updatedItem);
+                break;
+            }
+        }
+        save(filePath, data);
+    }
+    
+    protected void delete(String filePath, java.util.List<T> data, java.util.function.Predicate<T> predicate) throws java.io.IOException {
+        data.removeIf(predicate);
+        save(filePath, data);
+    }
+}
+
+class StudentFileSaver extends BaseFileSaver<Student> {
+    @Override
+    protected String formatLine(Student s) {
+        String subjects = String.join(";", s.getSubjectsEnrolled());
+        return String.join(",",
+            s.getStudentID(),
+            s.getStudentName(),
+            String.valueOf(s.getAge()),
+            s.getDateOfBirth(),
+            s.getYearLevel(),
+            s.getStudentType(),
+            subjects,
+            String.valueOf(s.getGwa()),
+            s.getEmail(),
+            s.getPhoneNumber(),
+            s.getGender(),
+            s.getAddress(),
+            s.getFathersName(),
+            s.getMothersName(),
+            s.getGuardiansPhoneNumber()
+        );
+    }
+}
+
+class ScheduleFileSaver extends BaseFileSaver<Schedule> {
+    @Override
+    protected String formatLine(Schedule s) {
+        return String.join(",",
+            s.getScheduleID(),
+            s.getCourseID(),
+            s.getRoom(),
+            s.getDay(),
+            s.getStartTime(),
+            s.getEndTime(),
+            s.getTeacherName()
+        );
+    }
+}
+
+class SectionFileSaver extends BaseFileSaver<Section> {
+    @Override
+    protected String formatLine(Section sec) {
+        String studentList = String.join(";", sec.getEnrolledStudentIDs());
+        return String.join(",",
+            sec.getSectionID(),
+            sec.getSubject().getCourseSubjectID(),
+            sec.getSchedule().getScheduleID(),
+            sec.getInstructor().getTeacherName(),
+            String.valueOf(sec.getCapacityLimit()),
+            String.valueOf(sec.getCurrentEnrollment()),
+            studentList
+        );
+    }
+}
+
+class CourseSubjectFileSaver extends BaseFileSaver<CourseSubject> {
+    @Override
+    protected String formatLine(CourseSubject course) {
+        String prereqs = course.getPrerequisitesString();
+        return String.join(",",
+            course.getCourseSubjectID(),
+            course.getCourseSubjectName(),
+            String.valueOf(course.getUnits()),
+            String.valueOf(course.getStudentCount()),
+            String.valueOf(course.isLabRoom()),
+            prereqs,
+            String.valueOf(course.getYearLevel()),
+            String.valueOf(course.getSemester())
+        );
+    }
+}
+
+class TeachersFileSaver extends BaseFileSaver<Teachers> {
+    @Override
+    protected String formatLine(Teachers teacher) {
+        String subjects = String.join(";", teacher.getQualifiedSubjectIDs());
+        String schedules = String.join(";", teacher.getAssignedSchedules());
+        return String.join(",",
+            teacher.getTeacherID(),
+            teacher.getTeacherName(),
+            subjects,
+            schedules
+        );
+    }
+}
+
+class RoomsFileSaver extends BaseFileSaver<Rooms> {
+    @Override
+    protected String formatLine(Rooms room) {
+        return String.join(",",
+            room.getRoomID(),
+            room.getRoomName(),
+            String.valueOf(room.getCapacity())
+        );
+    }
+}
+
+class EnrollmentFileSaver extends BaseFileSaver<Enrollment> {
+    @Override
+    protected String formatLine(Enrollment enrollment) {
+        return String.join(",",
+            enrollment.getStudentID(),
+            enrollment.getSectionID(),
+            enrollment.getCourseID()
+        );
+    }
+}
